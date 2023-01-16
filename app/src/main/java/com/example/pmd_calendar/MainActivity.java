1608b2ed97feb41c8a9b94df3b022c9278526455
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-01-16 10:54:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pmd_calendar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.pmd_calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonAceptar = (Button) findViewById(R.id.BotonAceptar);
+
+        botonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog fecha = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        TextView texto = (TextView) findViewById(R.id.textView);
+                        texto.setText(String.valueOf(month+"/"+day+"/"+year));
+                    }
+                },year,month,day);
+                fecha.show();
+            }
+        });
+
+
+
+
+
+    }
+}
